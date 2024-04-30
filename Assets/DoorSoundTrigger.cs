d0a3e053523f8fda7a5c7829124e1ce50a789403
fivenightsdave <75261394+davidcoom1@users.noreply.github.com>
2024-04-30 21:22:14 +0100

    More Sound fX

---
+++ Assets/DoorSoundTrigger.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSoundTrigger : MonoBehaviour
+{
+    private Vector3 lastPosition;
+    private Quaternion lastRotation;
+    public AudioSource audioSource; // Assign this in the inspector
+
+    void Start()
+    {
+        lastPosition = transform.position;
+        lastRotation = transform.rotation;
+    }
+
+    void Update()
+    {
+        // Check if the door has moved
+        if (transform.position != lastPosition || transform.rotation != lastRotation)
+        {
+            // Play the sound
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+
+            // Update last position and rotation
+            lastPosition = transform.position;
+            lastRotation = transform.rotation;
+        }
+    }
+}
---
+++ Assets/GameOverOnCollision.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // Include this for scene management
+
+public class GameOverOnCollision : MonoBehaviour
+{
+    public GameObject playerObject;
+    public float detectionDistance = 1.0f;
+
+    void Update()
+    {
+        if (playerObject == null)
+        {
+            Debug.Log("Player object is not assigned.");
+            return;
+        }
+
+        float currentDistance = Vector3.Distance(transform.position, playerObject.transform.position);
+        Debug.Log("Current distance to player: " + currentDistance); // Logs current distance each frame
+
+        if (currentDistance <= detectionDistance)
+        {
+            Debug.Log("Player has touched the black hole!");
+            EndGame();
+        }
+    }
+
+    private void EndGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
