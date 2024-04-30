a029dbcc4b9dcdab0b50a4bed8f2e19ade34efd4
fivenightsdave <75261394+davidcoom1@users.noreply.github.com>
2024-04-30 02:06:06 +0100

    Interactable 3

---
+++ Assets/BatteryCell.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryCell : MonoBehaviour
+{
+    // Flag to determine if this is the correct battery cell.
+    public bool isCorrect = false;
+}
---
+++ Assets/BatteryReceptacle.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryReceptacle : MonoBehaviour
+{
+    public GameObject door; // Reference to the door GameObject to control
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the object colliding with the receptacle is a battery cell
+        if (other.CompareTag("BatteryCell"))
+        {
+            // Get the BatteryCell component from the object
+            BatteryCell cell = other.GetComponent<BatteryCell>();
+            if (cell != null)
+            {
+                if (cell.isCorrect)
+                {
+                    // If the correct battery is inserted, open the door
+                    Debug.Log("Correct battery cell inserted, opening door...");
+                    if (door != null)
+                        door.GetComponent<DoorController>().OpenDoor();
+                }
+                else
+                {
+                    // If an incorrect battery is inserted, do nothing
+                    Debug.Log("Incorrect battery cell, nothing happens.");
+                }
+            }
+        }
+    }
+}
+
