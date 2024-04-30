152516f4490dcc53112e5d0c32e11aba4bb5d840
fivenightsdave <75261394+davidcoom1@users.noreply.github.com>
2024-04-30 22:23:00 +0100

    Done

--- Assets/PlaySoundOnTrigger.cs
+++ Assets/PlaySoundOnTrigger.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlaySoundOnTrigger : MonoBehaviour
 {
     private AudioSource audioSource;
+    private bool hasPlayed = false; // Flag to check if the sound has already been played
 
     void Start()
     {
@@ -13,10 +14,17 @@ public class PlaySoundOnTrigger : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("VRHand"))
+        // Check if the collider has the tag "VRHand" and if the sound has not been played yet
+        if (other.CompareTag("VRHand") && !hasPlayed)
         {
             audioSource.Play();
+            hasPlayed = true; // Set the flag to true to prevent further playback
         }
     }
-}
 
+    // Method to allow resetting the sound playback from other scripts or game events
+    public void ResetPlayback()
+    {
+        hasPlayed = false;
+    }
+}
